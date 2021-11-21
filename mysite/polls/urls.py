b3ca1b7765557d156bc38de2ee3a4ab593a1b7b3
jackPC <jl668124@hotmail.com>
2021-11-21 12:23:07 -0500

    implemented test cases,
    This will be a good state to go back and reference

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,15 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+
+'''
+urlpatterns = [
     path('', views.index, name='index'),
     
     
@@ -14,3 +23,4 @@ urlpatterns = [
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+'''
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,47 +1,45 @@
 
+
+
+
+
+
 from django.http import Http404
 from django.http import HttpResponse
+from django.utils import timezone
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
+from django.views import generic
 from django.urls import reverse
 from .models import Choice, Question
 
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request)) 
-    # a more direct way of doing the above without  httpResponce and loader
-    '''
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-    '''
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter( pub_date__lte=timezone.now() ).order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-
-    try:  # try allows the program to keep going with a error
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist: ## if statment analisysing above attempt
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
-
-# alternive of what was done above with get()
-'''
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})    
-'''
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte = timezone.now())
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 # currently does not handle race conditions like two people voting at once, can be fixed with code from the following link. https://docs.djangoproject.com/en/3.2/ref/models/expressions/#avoiding-race-conditions-using-f
@@ -64,3 +62,7 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
+
