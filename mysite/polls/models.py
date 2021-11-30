add1bde121244af25f932d7ed24ab78f85fd6211
jackPC <jl668124@hotmail.com>
2021-11-29 19:19:52 -0500

    slight changes

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,6 +13,10 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    # def was_published_recently(self):
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    
     @admin.display(
         boolean=True,
         ordering='pub_date',
@@ -22,7 +26,6 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
